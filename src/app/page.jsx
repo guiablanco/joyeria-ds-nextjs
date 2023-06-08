1511275569de54d7b72d2b3ae81a935718964cc0
Sebastián Aguirre <guiablanco@live.cl>
2023-06-07 21:14:56 -0400

    Añadiendo cards de la homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
+import LinksGallery from './components/linksGallery'
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Bienvenido a la web</h1>
+        <div>
+          <h1>Bienvenido a la web</h1>
+          <LinksGallery></LinksGallery>
+        </div>
 
       </div>
     </main>
